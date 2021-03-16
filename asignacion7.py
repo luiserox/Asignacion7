8d7fc73d15c0a2f09947b2fb649f125b2ef6875e
Luis E. Ruiz O <leruizoliveros@gmail.com>
2021-03-16 19:21:48 -0400

    Paso 5 - GET dispositivos de Cisco

--- asignacion7.py
+++ asignacion7.py
@@ -6,3 +6,11 @@ response = requests.post(
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
 payload=response.json()
 pprint(payload)
+
+dispositivos = requests.get(
+    'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device',
+    headers={'X-Auth-Token': response.json()['Token']}
+)
+
+for x in dispositivos.json()['response']:
+    print('\n\nTipo de equipo: ', x['family'], '\nHostname: ', x['hostname'], '\nDirección IP de administrador: ', x['managementIpAddress'], '\nFecha de Última Actualización: ', x['lastUpdated'], '\nEstatus del equipo: ', x['reachabilityStatus'])
